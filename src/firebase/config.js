0526d1b3d8ee878dea658054169fb1ec1d031709
JayKim88 <mearak43@gmail.com>
2021-04-09 23:32:59 +0900

    Apply dotenv for firebase API Key
    Hide all of sensitive info in .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,15 +2,17 @@ import firebase from "firebase/app";
 import "firebase/storage"; //store images
 import "firebase/firestore"; //database
 import "firebase/auth";
+import dotenv from "dotenv";
+dotenv.config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyBbUtdKCZqlOfa3U-PWLeFvE0LascCnLUw",
-  authDomain: "j-star-gram.firebaseapp.com",
-  projectId: "j-star-gram",
-  storageBucket: "j-star-gram.appspot.com",
-  messagingSenderId: "1088533633979",
-  appId: "1:1088533633979:web:8734c62f235b2d3944429e",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGESENDER,
+  appId: process.env.REACT_APP_APPID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
